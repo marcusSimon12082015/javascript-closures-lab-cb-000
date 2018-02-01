18fdc48ae88d10b1e4b05dc2f8cbd92d3168f002
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-02-01 18:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const app = "I don't do much."
 
 function bumpCounter(){
-
+  var counter = 0;
+  function addBump(){
+      counter++;
+  }
+  function getBumps(){
+    return counter;
+  }
+  return{
+    addBump,
+    getBumps
+  }
 }
